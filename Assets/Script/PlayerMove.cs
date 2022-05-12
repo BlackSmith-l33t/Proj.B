b81f6b047b83f6d7d950796f3e07c61359af08dc
Luke <blacksmith.l33t@gmail.com>
2022-05-12 21:49:21 +0900

    맵 구현 / 캐릭터 설정

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float playerSpeed;
+
+    Rigidbody rigid;
+
+    public int damage = 50;
+    public int health = 50;
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
- // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+
+        float hSpeed = hMove * playerSpeed * Time.deltaTime;
+        float vSpeed = vMove * playerSpeed * Time.deltaTime;
+
+        Vector2 vec = new Vector2(hSpeed, vSpeed);
+        rigid.velocity = vec;
     }
 }
    
