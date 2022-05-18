58892ac6cb1b450ba72bd873a718ef029d086eaa
Luke <blacksmith.l33t@gmail.com>
2022-05-18 15:49:54 +0900

    병합 오류 수정

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed;
+
+    Rigidbody rigid;
+
+    public int damage = 50;
+    public int health = 50;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+}
+   
