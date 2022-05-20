c8e63fa02d576637ebfcdc1a91de0ff6d1502563
Luke <blacksmith.l33t@gmail.com>
2022-05-20 16:05:29 +0900

    아이템 상호작용 구현 중

---
+++ Assets/Script/Box.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+
+    bool isTouch = false;
+
+
+    BoxCollider boxCollider;
+    Animator anim;
+
+    IEnumerator BoxChecker()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        if (isTouch)
+        {
+            
+        }
+    }
+
+    
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        boxCollider = GetComponent<BoxCollider>();
+    }    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {            
+            anim.SetTrigger("Touch");
+            Destroy(particleSystem);
+            isTouch = true;
+            Debug.Log("콰지직!");
+            Destroy(boxCollider);
+
+        }
+    }
+
+    //private void MoveBox()
+    //{
+    //    transform.position =  new
+    //}
+
+}
---
+++ Assets/Script/DevTool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevTool : MonoBehaviour
+{
+    GameObject directionalLight;
+
+    private void Awake()
+    {
+        directionalLight = GameObject.Find("Directional Light"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("z"))
+        {
+            directionalLight.SetActive(false);      
+        }
+    }
+}
