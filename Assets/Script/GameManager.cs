31d93fb6396b975809b62bdcaa0563d7ce32f3e8
Luke <blacksmith.l33t@gmail.com>
2022-05-23 20:34:21 +0900

    fade in/out 실험 1

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,8 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Invector.vCharacterController;
+using Invector.vCamera;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public Player player;
+    public GameObject deathUI;
+    public GameObject startPoint;
+    public Image fadeOutImage;
+    public vThirdPersonCamera mainCamera;
+    public vRagdoll deathMotion;
+
+    float fadeTime = 5.0f; // fade 효과 재생시간
+    float start;
+    float end;
+    float time = 0.0f;
+
+    bool isPlaying = false;
+
+    private void Awake()
+    {        
+        player.OnDie += Death;
+    }
+
+    IEnumerator StartFadeIn()
+    {
+        yield return new WaitForSeconds(4.0f);
+
+        //if (player.isAlive)
+        //{
+        //    yield return null;
+        //}
+
+        isPlaying = true;        
+
+        Color fadeColor = fadeOutImage.color;
+        time = 0.0f;        
+        fadeColor.a = Mathf.Lerp(start, end, time);
+
+        while (fadeColor.a > 0.0f)
+        {
+            time += Time.deltaTime / fadeTime;
+            fadeColor.a = Mathf.Lerp(start, end, time);
+            fadeOutImage.color = fadeColor;
+            yield return null;
+        }
+
+        isPlaying = false;
+        Restart();
+    }  
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Pit");        
+    }
+
+    private void Death()
+    {
+        deathMotion.ActivateRagdoll();
+        deathUI.SetActive(true);
+        mainCamera.GetComponentInChildren<GraySceleEffect>().enabled = true;
+        mainCamera.GetComponentInChildren<vThirdPersonCamera>().isFreezed = true;
+
+        Debug.Log("fade out 호출");
+        // TODO :흑백 화면 -> UI 표시 -> fade out -> ReStart() 호출
+        FadeIn();
+    }
+
+    public void FadeIn()
+    {
+        if (true == isPlaying) // 중복 재생 방지
+        {
+            return;
+        }
+        start = 5.0f;
+        end = 0.0f;
+
+        StartCoroutine("StartFadeIn");
+    }
 }
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Invector.vCharacterController;
+using Invector.vCamera;
+using UnityEngine.Events;
+
+public class Player: MonoBehaviour
+{
+    public event UnityAction OnDie;
+    public event UnityAction OnScore;
+    public bool isAlive = true;       
+
+    int BombBoxLayer = 19;   
+ 
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("플레이어 충돌 감지");
+        if (!(other.gameObject.layer == BombBoxLayer))
+        {
+            return;
+        }
+
+        isAlive = false;
+        OnDie?.Invoke();
+        Debug.Log("You Die");
+    }
+}
+   
