b3d0005b5c22c01b49ab445853645c5ddd1585a8
Luke <blacksmith.l33t@gmail.com>
2022-05-24 16:31:15 +0900

    onDead Test
    onDead Test를 위한 백업 1

--- Assets/Script/Prisoner.cs
+++ Assets/Script/Prisoner.cs
@@ -6,29 +6,34 @@ public class Prisoner: MonoBehaviour
 {
     public event UnityAction OnDie;
     public event UnityAction OnScore;
-    public vThirdPersonController playerController;
-    public bool isAlive = true;       
+    public bool isAlive = true;
+
+    vThirdPersonController playerController;
 
     int BombBoxLayer = 19;
     int enemy = 9;
     int damage = 10;
 
+    private void Awake()
+    {
+        playerController = GetComponent<vThirdPersonController>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("플레이어 충돌 감지");
-        if (!(other.gameObject.layer == BombBoxLayer))
+        if (other.gameObject.layer == BombBoxLayer)
         {
-            return;
+            isAlive = false;
+            OnDie?.Invoke();
+            Debug.Log("You Die");
+            Debug.Log(playerController.currentHealth);            
         }
         else if (other.gameObject.layer == enemy)
         {
             playerController.currentHealth -= damage;
-            Debug.Log(playerController.currentHealth);
-        }
-
-        isAlive = false;
-        OnDie?.Invoke();
-        Debug.Log("You Die");
+            Debug.Log(playerController.currentHealth);            
+        }    
     }
 }
    
